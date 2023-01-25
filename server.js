5640417ad2b56141ab8b9ecf94b3e1b773c85e0d
mathur619 <prakhar@fyno.io>
2023-01-25 08:26:42 +0530

    Initial express setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
+  )
+);
