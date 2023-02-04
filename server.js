4f17df3dc4c68662cfed216b1fbe932375f295ba
mathur619 <prakhar@fyno.io>
2023-02-04 20:12:31 +0530

    Hanlde unhandled promise rejections

--- server.js
+++ server.js
@@ -21,9 +21,17 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 6000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
   )
 );
+
+// Hanlde unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // close server and exit process
+  server.close(() => process.exit(1));
+});
