f58d4a92da49506f4fda70185bc63410ebb0dc12
mathur619 <prakhar@fyno.io>
2023-04-08 13:32:19 +0530

    used statusCode in errorHandler

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   console.log(err.stack.red);
 
-  res.status(500).json({
+  res.status(err.statusCode).json({
     success: false,
     error: err.message,
   });
